cd08894339a85bc5a90a5e87523da187519b6105
Victor <victorlopezruiz@eluig.xeill.net>
2026-04-17 11:25:33 +0200

    Fix: Update CORS with Vercel URL

--- conflict-tracker-victor-lopez-master/conflict-tracker-victor-lopez/src/main/java/com/backend/apirest/config/CorsConfig.java
+++ conflict-tracker-victor-lopez-master/conflict-tracker-victor-lopez/src/main/java/com/backend/apirest/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://conflict-tracker-frontend-eight.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
